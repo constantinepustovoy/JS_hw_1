0087865eaf6ee82a4e142acbbbdf482069f8bc25
Constantine Pustovoy <pustovoylug@gmail.com>
2016-08-18 17:45:55 +0300

    hw_1_1

--- js/function.js
+++ js/function.js
@@ -2,37 +2,30 @@
 // console.log(alert)
 
 var number = prompt('Укажите число', 1);
-console.log(typeof number);
 var number = parseInt(number);
-console.log(typeof number);
 console.log(number);
 var grade = prompt('Укажите степерь возведения', 1);
 var grade = parseInt(grade);
 console.log(grade);
-var i = 0;
-var result = number;
 
-function pow(number, grade, i, result) {
-	if (number > 0 && grade > 0) {
-		for (var i = 0; i <= grad; i++) {
-			alert(i);
+function pow(number, grade) {
+	var result = 1;
+	if (number === 0) {
+		return 0
+		} else  if (grade === 0) {
+		return 1;
+	} 
+	
+	for (var i = 0; i < Math.abs(grade); i++) {
 		result = result * number;
-		return result;
-		}
-	} else if (grade == 0) {
-		result = 1;
-		return result;
-	} else if (number == 0){
-		result = 0;
-		return result
-	} else if (grade < 0){
-		while (i <= grade) {
-		result *= number;
-		i++;
-	}
-		result = 1 / result;
-		return result;
 	}
 
-}
-	console.log(result);
+	if (grade < 0) {
+		result = 1/result;
+	}
+	return result
+	}
+
+	pow(number, grade);
+	console.log( pow(number, grade) );
+	
